132c17ebbd1f62714a23359d0bcbc4c90f57bd79
Yujun <yujuncho7@gmail.com>
2021-05-20 13:12:31 -0700

    Completed section 3 on react basics and working with components

---
+++ src/components/expenses/Expenses.js
@@ -0,0 +1,20 @@
+import Card from "../ui/Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      {props.items.map((item, index) => (
+        <ExpenseItem
+          key={index}
+          title={item.title}
+          amount={item.amount}
+          date={item.date}
+        />
+      ))}
+    </Card>
+  );
+}
+
+export default Expenses;
